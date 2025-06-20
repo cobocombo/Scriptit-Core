570f6b93cf928baf4597c541b87f8665605202b7
Colton <coltonboyd503@icloud.com>
2025-06-20 09:39:15 -0400

    Implemented basic listening to console.log statements and errors. Closes #4.

--- Source/Scriptit Core/Scriptit Core/CoreController.swift
+++ Source/Scriptit Core/Scriptit Core/CoreController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import WebKit
 
-class CoreController: UIViewController
+class CoreController: UIViewController, WKScriptMessageHandler
 {
   override func viewDidLoad()
   {
@@ -9,8 +9,43 @@ class CoreController: UIViewController
     
     let userContentController = WKUserContentController();
     
+    // Inject JS to capture console.log
+    let consoleLogScript = """
+    (function() {
+      var oldLog = console.log;
+      console.log = function(...args) {
+        window.webkit.messageHandlers.consoleLog.postMessage(args.join(" "));
+        oldLog.apply(console, args);
+      };
+    })();
+    """
+    
+    // Inject JS to capture window.onerror (uncaught errors)
+    let errorCaptureScript = """
+    window.onerror = function(message, source, lineno, colno, error) {
+      window.webkit.messageHandlers.jsError.postMessage(
+        'JS Error: ' + message + ' at ' + source + ':' + lineno + ':' + colno
+      );
+    };
+    """
+    
+    // Add the scripts to the controller
+    let logUserScript = WKUserScript(source: consoleLogScript, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+    let errorUserScript = WKUserScript(source: errorCaptureScript, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+    
+    userContentController.addUserScript(logUserScript)
+    userContentController.addUserScript(errorUserScript)
+
+    // Register handlers for messages
+    userContentController.add(self, name: "consoleLog")
+    userContentController.add(self, name: "jsError")
+    
+    let preferences = WKPreferences()
+    preferences.setValue(true, forKey: "developerExtrasEnabled")
+    
     let webViewConfig = WKWebViewConfiguration();
     webViewConfig.userContentController = userContentController;
+    webViewConfig.preferences = preferences
     
     let webView = WKWebView(frame: view.bounds, configuration: webViewConfig);
     webView.autoresizingMask = [ .flexibleWidth, .flexibleHeight];
@@ -21,6 +56,19 @@ class CoreController: UIViewController
       let fileDirectory = fileURL.deletingLastPathComponent();
       webView.loadFileURL(fileURL, allowingReadAccessTo: fileDirectory);
       self.view.addSubview(webView);
+    }
+  }
+  
+  func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage)
+  {
+    switch message.name
+    {
+      case "consoleLog":
+        print("JS console.log: \(message.body)")
+      case "jsError":
+        print("JS Error:", message.body)
+      default:
+        break
     }
   }
 }
