99173d6cf8b7e0b812d745e083f170e16bac6f98
Colton <coltonboyd503@icloud.com>
2025-07-08 23:30:13 -0400

    Fixed bug where phaser doesnt load local images. Closes #40.

--- Source/Scriptit Core/Scriptit Core/Core.swift
+++ Source/Scriptit Core/Scriptit Core/Core.swift
@@ -27,6 +27,7 @@ class ScriptitCoreController: UIViewController, WKScriptMessageHandler
     
     let preferences = WKPreferences();
     preferences.setValue(true, forKey: "developerExtrasEnabled");
+    preferences.setValue(true, forKey: "allowFileAccessFromFileURLs");
     
     let userContentController = WKUserContentController();
     userContentController.add(self, name: "consoleMessageManager");
