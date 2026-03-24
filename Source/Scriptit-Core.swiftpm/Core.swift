0fb39fb47b397f963458e7b8194da11aa372287a
cobocombo <105602997+cobocombo@users.noreply.github.com>
2026-03-23 21:23:45 -0400

    Support light only version of progress hud.

--- Source/Scriptit-Core.swiftpm/Core.swift
+++ Source/Scriptit-Core.swiftpm/Core.swift
@@ -2430,6 +2430,14 @@ class HudMessageManager: JavascriptMessageManager
    */
   func handleMessage(_ message: WKScriptMessage, webView: WKWebView)
   {
+    if let windowScene = UIApplication.shared.connectedScenes
+      .compactMap({ $0 as? UIWindowScene })
+      .first,
+       let window = windowScene.windows.first(where: { $0.isKeyWindow })
+    {
+      window.overrideUserInterfaceStyle = .light
+    }
+    
     guard let dict = message.body as? [String: Any] else
     {
       let error = self.errors.invalidMessageBody.rawValue;
--- Source/Scriptit-Core.swiftpm/Resources/main.js
+++ Source/Scriptit-Core.swiftpm/Resources/main.js
@@ -7,18 +7,7 @@ class HomePage extends ui.Page
   
   onInit()
   {
-    // files.deleteFile({ subpath: 'trash.js' });
-    // files.deleteFile({ subpath: 'trash(copy).js' });
-    files.copyFile({ oldRoot: files.roots.bundle, oldSubpath: 'trash.js', copiedFileName: 'trash.js' })
-    .then(file => 
-    {
-      console.log(file);
-      files.readFile({ subpath: file.relativePath })
-      .then(content => 
-      {
-        console.log(content);
-      });
-    });
+    
   }
 }
 
