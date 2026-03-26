1c3a7611944d1d18edb4a709fa38ca52f1e19912
cobocombo <105602997+cobocombo@users.noreply.github.com>
2026-03-26 00:16:14 -0400

    Closes #102.

--- Source/Scriptit-Core.swiftpm/Resources/main.js
+++ Source/Scriptit-Core.swiftpm/Resources/main.js
@@ -6,9 +6,16 @@ class HomePage extends ui.Page
   }
   
   onInit()
-  {
+  { 
+    let code = new ui.Codeblock({ code: js });
+    code.font = code.fonts.systemMono;
+    code.fontSize = '15px';
+    code.theme = code.themes.xcodeLight;
+    code.marginLeft = '12px';
     
+    this.addComponents({ components: [ code ] });
   }
 }
 
+let js = `console.log('Hello World!');\nlet text = 'Hi!'\nconsole.log('Hello World!');`;
 app.present({ root: new HomePage() });
